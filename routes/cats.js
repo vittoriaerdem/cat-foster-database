066050e9d6326400ac4959a517e63d6b18a4e22a
Vittoria Erdem <vittoriaerdem@gmail.com>
2019-08-30 12:24:26 -0500

    add log in authenticator to pages restricted to only logged in users

--- routes/cats.js
+++ routes/cats.js
@@ -3,9 +3,14 @@ var router = express.Router();
 var catCtrl = require('../controllers/cats');
 
 router.get('/', catCtrl.index)                   // SHOWS ALL CATS
-router.get('/new', catCtrl.new);                 // ADD A NEW CAT TO DATABASE
-router.post('/', catCtrl.create);                // UPDATE DATABASE WITH NEW CAT
-router.get('/:id', catCtrl.show);                // SHOW DETAILS OF ONE CAT
-router.delete('/index/:id', catCtrl.delete);     // DELETE A CAT FROM THE DATABASE
+router.get('/new', isLoggedIn, catCtrl.new);                 // ADD A NEW CAT TO DATABASE
+router.post('/',isLoggedIn, catCtrl.create);                // UPDATE DATABASE WITH NEW CAT
+router.get('/:id',isLoggedIn, catCtrl.show);                // SHOW DETAILS OF ONE CAT
+router.delete('/index/:id', isLoggedIn, catCtrl.delete);     // DELETE A CAT FROM THE DATABASE
+
+function isLoggedIn(req, res, next) {
+  if ( req.isAuthenticated() ) return next();
+  res.redirect('/auth/google');
+}
 
 module.exports = router;
